1a40c115be277816f54389fdebf6632c71fa288e
dmitriy98bondarenko-dev <dmitriy98bondarenko@gmail.com>
2025-10-28 23:12:39 +0100

    refactor(script-engine): move async promise handling from postman to tool

--- js/feature.js
+++ js/feature.js
@@ -1058,7 +1058,7 @@ function toggleWelcomeCard(show) {
         card.hidden = !show;
     }
 }
-// one time run auth script
+//one time run auth script
 async function runCollectionAuth() {
     try {
         const code = (state.COLLECTION?.event || [])
@@ -1242,7 +1242,6 @@ export async function bootApp({ collectionPath, autoOpenFirst }) {
             if (filterInp.value) applyFilter();
         }
     }
-
     //  env dropdown
     initEnvDropdown();
 
