ac418ea99be3f877598f945a1fe67ae13dd61487
Dmytro Bondarenko <dmytro.bondarenko@uklon.com.ua>
2025-11-19 11:39:56 +0000

    Edit config.js

--- js/config.js
+++ js/config.js
@@ -106,7 +106,7 @@ export function fetchWithTimeout(url, opts = {}, ms = REQUEST_TIMEOUT_MS) {
 /**
  *  dg proxy config
  * */
-const PROXY_URL = 'https://driver.dev.uklon.com.ua';
+const PROXY_URL = 'https://driver.dev.uklon.com.ua/';
 const PROXY_DOMAINS = ['localhost', 'api-helper-cee777.pages.uklon.net'];
 const REQUEST_TIMEOUT_MS = 15000;
 
